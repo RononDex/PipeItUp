2e48172370f5cc9213901538075b52481acdcba0
RononDex <tinoheuberger@protonmail.com>
2021-11-25 17:55:42 +0100

    VT122106-55: Add missing functions.sh

---
+++ pi/functions.sh
@@ -0,0 +1,10 @@
+# $1: Wifi SSID
+# $2: Wifi Password
+SetupKnownWifi() {
+
+	if grep -q "ssid=$1" "/etc/wpa_supplicant.conf"; then
+		echo "Wifi with SSID $1 already known"
+	else
+		wpa_passphrase $1 $2 | sudo tee /etc/wpa_supplicant.conf
+	fi
+}
