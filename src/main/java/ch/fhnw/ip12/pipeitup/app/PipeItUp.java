1f55b5700127eae15f138c0364e49036e227150b
RononDex <tinoheuberger@protonmail.com>
2021-11-29 11:44:03 +0100

    VT122106-57: Setup basic maven project

---
+++ src/main/java/ch/fhnw/ip12/pipeitup/app/PipeItUp.java
@@ -0,0 +1,12 @@
+package main.java.ch.fhnw.ip12.pipeitup.app;
+
+/**
+* PipeItUp
+*/
+public class PipeItUp {
+
+	public static void main(String[] args) {
+		System.out.println("Pipe-It-Up is awesome!");
+	}
+	
+}
