56440757249f0eefe331ca34960ba8ffc69c6143
RononDex <tinoheuberger@protonmail.com>
2021-11-29 14:28:38 +0100

    VT122106-57: Setup sample JavaFX app

---
+++ PipeItUp/src/main/java/ch/fhnw/ip12/pipeitup/app/PipeItUp.java
@@ -0,0 +1,35 @@
+package ch.fhnw.ip12.pipeitup.app;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * PipeItUp
+ */
+public class PipeItUp extends Application {
+
+	public static void main(String[] args) {
+		System.out.println("Pipe-It-Up is awesome!");
+
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Group root = new Group();
+		Scene scene = new Scene(root);
+
+		//Setting the title to Stage. 
+		primaryStage.setTitle("Sample application"); 
+			
+		//Setting the scene to Stage 
+		primaryStage.setScene(scene); 
+			
+		//Displaying the stage 
+		primaryStage.show();
+	}
+
+}
