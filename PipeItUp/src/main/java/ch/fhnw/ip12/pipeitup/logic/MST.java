ddde4c754baf226ded3f16f9d778e1b5dc254cdf
GimliTheWise <carl.vonburg@students.fhnw.ch>
2021-11-30 12:56:52 +0100

    MST changes

--- PipeItUp/src/main/java/ch/fhnw/ip12/pipeitup/logic/MST.java
+++ PipeItUp/src/main/java/ch/fhnw/ip12/pipeitup/logic/MST.java
@@ -13,14 +13,29 @@ public abstract class MST {
 		// TODO: instantiate vertices and edges
 	}
 
+	/**
+	 * Check if the edge can be used in the process of finding the mst
+	 * @param edge to check usability for
+	 * @return true if next possible edge contains the given edge
+	 */
 	public boolean isUsableEdge(Edge edge){
 		return Arrays.asList(nextPossibleEdges()).contains(edge);
 
 	}
 
+	/**
+	 * Returns the next possible edge, dependent on the algorithm used.
+	 * @return null if no possible edges, otherwise returns all possible edges to use for mst
+	 */
 	abstract Edge[] nextPossibleEdges();
 
-
+	/**
+	 * check if all vertices are used
+	 * @return true if all vertices are used, otherwise false
+	 */
+	public boolean isAchieved(){
+		return nextPossibleEdges() == null;
+	}
 
 	public int sum(){
 		// TODO: return sum of edge weights, only when all vertices are used.
