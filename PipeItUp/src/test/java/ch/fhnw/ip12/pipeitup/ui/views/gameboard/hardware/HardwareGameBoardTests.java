0a84b883f60cc7c3d52618e5d2c22ab07f3200d0
RononDex <tinoheuberger@protonmail.com>
2021-11-30 10:55:24 +0100

    VT122106-57: Create basic UI logic

---
+++ PipeItUp/src/test/java/ch/fhnw/ip12/pipeitup/ui/views/gameboard/hardware/HardwareGameBoardTests.java
@@ -0,0 +1,18 @@
+package ch.fhnw.ip12.pipeitup.ui.views.gameboard.hardware;
+
+import org.junit.Test;
+
+/**
+* HardwareGameBoard
+*/
+public class HardwareGameBoardTests {
+
+	@Test
+	public void Test()
+	{
+		HardwareGameBoard gameBoard = new HardwareGameBoard();
+		gameBoard.start();
+	}
+	
+	
+}
