08238f9d05dcedaebd8e5e9f9b1794d9e8cecac3
RononDex <tinoheuberger@protonmail.com>
2022-03-02 16:55:37 +0100

    WIP work on tests

--- PipeItUp/src/test/java/ch/fhnw/ip12/pipeitup/logic/PrimTest.java
+++ PipeItUp/src/test/java/ch/fhnw/ip12/pipeitup/logic/PrimTest.java
@@ -1,61 +1,57 @@
 package ch.fhnw.ip12.pipeitup.logic;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import ch.fhnw.ip12.pipeitup.logic.Models.EdgeModel;
+import ch.fhnw.ip12.pipeitup.logic.Models.GraphLayoutModel;
+import ch.fhnw.ip12.pipeitup.logic.Models.VertexModel;
 
 class PrimTest {
 
-	Vertex v1 = new Vertex();
-	Vertex v2 = new Vertex();
-	Vertex v3 = new Vertex();
-	Vertex v4 = new Vertex();
-	Edge e1 = new Edge(1, new HashSet<>(){{add(v1);add(v2);}});
-	Edge e2 = new Edge(6, new HashSet<>(){{add(v1);add(v3);}});
-	Edge e3 = new Edge(7, new HashSet<>(){{add(v1);add(v4);}});
-	Edge e4 = new Edge(4, new HashSet<>(){{add(v2);add(v4);}});
-	Edge e5 = new Edge(3, new HashSet<>(){{add(v3);add(v4);}});
-	Graph graph = new Graph(new LinkedHashSet<>(Arrays.asList(e1,e2,e3,e4,e5)));
-	Prim prim = new Prim(graph, v1);
+	VertexModel v1 = new VertexModel(0, 1);
+	VertexModel v2 = new VertexModel(0, 2);
+	VertexModel v3 = new VertexModel(0, 3);
+	VertexModel v4 = new VertexModel(0, 4);
+	EdgeModel e1 = new EdgeModel(v1, v2, 1);
+	EdgeModel e2 = new EdgeModel(v1, v3, 6);
+	EdgeModel e3 = new EdgeModel(v1, v4, 7);
+	EdgeModel e4 = new EdgeModel(v2, v4, 4);
+	EdgeModel e5 = new EdgeModel(v3, v4, 3);
+	GraphLayoutModel graph = new GraphLayoutModel(Arrays.asList(v1, v2, v3, v4), Arrays.asList(e1, e2, e3, e4, e5));
 
 	@Test
 	void isNextEdge() {
-		v1.addEdge(e1);
-		v1.addEdge(e2);
-		v1.addEdge(e3);
-		v2.addEdge(e1);
-		v2.addEdge(e4);
-		v3.addEdge(e2);
-		v3.addEdge(e5);
-		v4.addEdge(e3);
-		v4.addEdge(e4);
-		v4.addEdge(e5);
+		Prim testee = new Prim();
+		testee.setStartVertex(start);
 
-		assertTrue(prim.isNextEdge(e1));
-		assertFalse(prim.isNextEdge(e2));
-		assertFalse(prim.isNextEdge(e3));
-		assertFalse(prim.isNextEdge(e4));
-		assertFalse(prim.isNextEdge(e5));
+		assertTrue(testee.isNextEdge(e1));
+		assertFalse(testee.isNextEdge(e2));
+		assertFalse(testee.isNextEdge(e3));
+		assertFalse(testee.isNextEdge(e4));
+		assertFalse(testee.isNextEdge(e5));
 		e1.setUsed(true);
 		v1.setVisited(true);
 		v2.setVisited(true);
-		assertFalse(prim.isNextEdge(e1));
-		assertFalse(prim.isNextEdge(e2));
-		assertFalse(prim.isNextEdge(e3));
-		assertTrue(prim.isNextEdge(e4));
-		assertFalse(prim.isNextEdge(e5));
+		assertFalse(testee.isNextEdge(e1));
+		assertFalse(testee.isNextEdge(e2));
+		assertFalse(testee.isNextEdge(e3));
+		assertTrue(testee.isNextEdge(e4));
+		assertFalse(testee.isNextEdge(e5));
 		e4.setUsed(true);
 		v4.setVisited(true);
 		e5.setUsed(true);
 		v3.setVisited(true);
-		assertFalse(prim.isNextEdge(e1));
-		assertFalse(prim.isNextEdge(e2));
-		assertFalse(prim.isNextEdge(e3));
-		assertFalse(prim.isNextEdge(e4));
-		assertFalse(prim.isNextEdge(e5));
+		assertFalse(testee.isNextEdge(e1));
+		assertFalse(testee.isNextEdge(e2));
+		assertFalse(testee.isNextEdge(e3));
+		assertFalse(testee.isNextEdge(e4));
+		assertFalse(testee.isNextEdge(e5));
 	}
 }
